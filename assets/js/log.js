5a288c11aa4e310ab9e5b0dfba88ebe022c705ac
syurecat <syurecat0@gmail.com>
2024-05-03 00:59:16 +0900

    major changes log

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,31 +1,9 @@
-'use strict';
-
-const gasURL = 'https://script.google.com/macros/s/AKfycbyeHWh-dbz4Z9r9hy4dm1yh2V66pH2DqPtEgCeusIWK-P0WdhuWtxObicpsHuL7AlWh/exec';
-const events = [
-    'request.detail.show',
-    'request.judgement.show'
-];
-
-collaboflow.events.on(events, function(e) {
-    const fetchIpAddress = async() => {
-        return await fetch('https://ipinfo.io?callback')
-        .then(response => response.json())
-        .then(json => json.ip);
-    }
-
-    const writeAccessLog = async() => {
-        const ipAddress = await fetchIpAddress();
-        const loginUser = collaboflow.getLoginUser();
-        const logData = {
-            'ip_address' : ipAddress,
-            'user_agent' : navigator.userAgent
-        };
-
-        collaboflow.proxy.post(gasURL, {}, logData, 'json')
-        .then(response => {
-            console.log('logging', response);
-        });
-    }
-
-    writeAccessLog();
-});
+const gasURL = 'https://script.google.com/macros/s/AKfycbxNMugKJ_Itx0_Clp63EsM8NSd9ExpGXlpGLJqExlDmTEuZEL5Ed-VubKtZV8SEo7ExUw/exec';
+const fetchIpAddress = async() => {
+    return await fetch('https://ipinfo.io?callback')
+    .then(response => response.json())
+    .then(json => json.ip);
+}
+console.log(fetchIpAddress);
+const userAgent = navigator.userAgent;
+console.log(userAgent);
