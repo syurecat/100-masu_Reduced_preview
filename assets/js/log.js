207bed3e91d066a01851cc5f904c127850301f59
syurecat <syurecat0@gmail.com>
2024-05-04 01:30:18 +0900

    fix userip

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,7 +1,7 @@
 const gasURL = 'https://script.google.com/macros/s/AKfycbxNMugKJ_Itx0_Clp63EsM8NSd9ExpGXlpGLJqExlDmTEuZEL5Ed-VubKtZV8SEo7ExUw/exec';
-const fetchIpAddress = await fetch('https://domains.google.com/checkip')
-const userIp = await fetchIpAddress.json()
-console.log(userIp);
+const promise = fetch('https://icanhazip.com');
+const userIp = promise.txt();
+console.log(promise);
 const userAgent = navigator.userAgent;
 console.log(userAgent);
 const userData = {
@@ -9,4 +9,3 @@ const userData = {
     "agent": userAgent
 };
 doPost(gasURL, userData);
-https://domains.google.com/checkip
