a5d692070b3fbec8fde9ff990bec0a962754652f
syurecat <syurecat0@gmail.com>
2024-05-05 00:41:43 +0900

    delete comment

--- assets/js/log.js
+++ assets/js/log.js
@@ -13,7 +13,7 @@ async function dogasreq(url, data) {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        return response.json(); // Assuming server returns JSON response
+        return response.json();
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error.message);
         return null;
