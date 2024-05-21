79712875fa8514ebe51aad648f2449d17b1db198
syurecat <syurecat0@gmail.com>
2024-05-22 01:45:21 +0900

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
+function start() {
+    var quiz_x = generate_random_quiz;
+    var quiz_y = generate_random_quiz;
+
+    quiz_x.forEach((item, index) => {
+        const cellId = `quiz_x${index + 1}`
+        const cell = document.getElementById(cellId);
+        if (cell) cell.textContent = item;
+    });
+    quiz_y.forEach((item, index) => {
+        const cellId = `quiz_y${index + 1}`
+        const cell = document.getElementById(cellId);
+        if (cell) cell.textContent = item;
+    });
+
+    document.getElementById(bn_start).innerText = "Enter";
+    document.getElementById(bn_start).onclick = check()
+}
+
+function generate_random_quiz() {
+    let ql = [0,1,2,3,4,5,6,7,8,9];
+    let a = ql.length;
+    while (a) {
+        let j = Math.floor( Math.random() * a );
+        let t = ql[--a];
+        ql[a] = ql[j];
+        ql[j] = t;
+    }
+    return ql
+}
+
 function doPost(url, data) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url);
